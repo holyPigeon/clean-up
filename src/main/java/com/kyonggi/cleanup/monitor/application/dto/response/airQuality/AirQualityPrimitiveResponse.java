4b5c149919ae286fad41d6e5bb3cd337fe38b658
jae woo Jung <awfjol2008@naver.com>
2024-02-08 16:46:06 +0900

    feat: 대기오염정보 API response dto에 측정자료 상태정보를 나타내는 Flag 관련 필드 추가

--- src/main/java/com/kyonggi/cleanup/monitor/application/dto/response/airQuality/AirQualityPrimitiveResponse.java
+++ src/main/java/com/kyonggi/cleanup/monitor/application/dto/response/airQuality/AirQualityPrimitiveResponse.java
@@ -19,29 +19,35 @@ public class AirQualityPrimitiveResponse {
     public static class Item {
 
         // 미세먼지
+        private String pm10Flag;
         private Integer pm10Value;
         private Integer pm10Value24;
         private Integer pm10Grade;
         private Integer pm10Grade1h;
 
+        private String pm25Flag;
         private Integer pm25Value;
         private Integer pm25Value24;
         private Integer pm25Grade;
         private Integer pm25Grade1h;
 
         // 기타
+        private String so2Flag;
         private Double so2Value;
         private Integer so2Grade;
 
+        private String o3Flag;
         private Double o3Value;
         private Integer o3Grade;
 
         private Integer khaiValue;
         private Integer khaiGrade;
 
+        private String no2Flag;
         private Double no2Value;
         private Integer no2Grade;
 
+        private String coFlag;
         private Double coValue;
         private Integer coGrade;
 
