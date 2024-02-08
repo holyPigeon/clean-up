b881ab298f67352e740032a270b058ebb1a2e75d
jae woo Jung <awfjol2008@naver.com>
2024-02-08 10:46:02 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/kyonggi/cleanup/CleanUpApplication.java
@@ -0,0 +1,13 @@
+package com.kyonggi.cleanup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kyonggi/cleanup/CleanUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyonggi.cleanup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
