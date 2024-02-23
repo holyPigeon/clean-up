2a78d9a4e651b321d1bb7bad5926ff30450020be
jae woo Jung <awfjol2008@naver.com>
2024-02-23 12:13:41 +0900

    feat: 대기오염정보 요청 및 반환 API 구현

--- src/main/java/com/kyonggi/cleanup/monitor/presentation/WeatherController.java
+++ src/main/java/com/kyonggi/cleanup/monitor/presentation/WeatherController.java
@@ -1,10 +1,33 @@
 package com.kyonggi.cleanup.monitor.presentation;
 
+import com.kyonggi.cleanup.common.domain.response.ResponseHandler;
+import com.kyonggi.cleanup.monitor.application.WeatherService;
+import com.kyonggi.cleanup.monitor.application.dto.response.airQuality.AirQualityResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class WeatherController {
 
+    private final WeatherService weatherService;
+
+    @GetMapping(value = "/weather/airQuality/forecast", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseHandler<AirQualityResponse>> fetchAirQuality() {
+
+        AirQualityResponse airQualityResponse = weatherService.fetchAirQualityInfo();
+
+        return ResponseEntity
+                .ok()
+                .body(ResponseHandler.<AirQualityResponse>builder()
+                        .message("Success")
+                        .statusCode(HttpStatus.OK)
+                        .data(airQualityResponse)
+                        .build()
+                );
+    }
 }
