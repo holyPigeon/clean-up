ac47b6764922365670cdbadaeddf4a415b2fcc86
jae woo Jung <awfjol2008@naver.com>
2024-02-23 11:54:46 +0900

    fix: 대기오염정보 원본 응답 필드 타입 변경
    타입을 Integer 및 Double로 설정한 다양한 필드에서 Integer, Double 값 이외의 문자열 값이 들어오는 것을 확인하여 해당 타입을 모두 String으로 변경

--- src/main/java/com/kyonggi/cleanup/monitor/application/dto/response/airQuality/AirQualityPrimitiveResponse.java
+++ src/main/java/com/kyonggi/cleanup/monitor/application/dto/response/airQuality/AirQualityPrimitiveResponse.java
@@ -1,6 +1,5 @@
 package com.kyonggi.cleanup.monitor.application.dto.response.airQuality;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,7 +7,6 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class AirQualityPrimitiveResponse {
 
     private ResponseBody response;
@@ -26,49 +24,50 @@ public class AirQualityPrimitiveResponse {
             private int totalCount;
             private List<Item> items;
 
-    @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class Item {
+            @Getter
+            @NoArgsConstructor
+            public static class Item {
 
-        // 미세먼지
-        private String pm10Flag;
-        private Integer pm10Value;
-        private Integer pm10Value24;
-        private Integer pm10Grade;
-        private Integer pm10Grade1h;
+                // 미세먼지
+                private String pm10Flag;
+                private String pm10Value;
+                private String pm10Value24;
+                private String pm10Grade;
+                private String pm10Grade1h;
 
-        private String pm25Flag;
-        private Integer pm25Value;
-        private Integer pm25Value24;
-        private Integer pm25Grade;
-        private Integer pm25Grade1h;
+                private String pm25Flag;
+                private String pm25Value;
+                private String pm25Value24;
+                private String pm25Grade;
+                private String pm25Grade1h;
 
-        // 기타
-        private String so2Flag;
-        private Double so2Value;
-        private Integer so2Grade;
+                // 기타
+                private String so2Flag;
+                private String so2Value;
+                private String so2Grade;
 
-        private String o3Flag;
-        private Double o3Value;
-        private Integer o3Grade;
+                private String o3Flag;
+                private String o3Value;
+                private String o3Grade;
 
-        private Integer khaiValue;
-        private Integer khaiGrade;
+                private String khaiValue;
+                private String khaiGrade;
 
-        private String no2Flag;
-        private Double no2Value;
-        private Integer no2Grade;
+                private String no2Flag;
+                private String no2Value;
+                private String no2Grade;
 
-        private String coFlag;
-        private Double coValue;
-        private Integer coGrade;
+                private String coFlag;
+                private String coValue;
+                private String coGrade;
 
-        private String stationName;
-        private String stationCode;
+                private String stationName;
+                private String stationCode;
 
-        private String mangName;
-        private String sidoName;
-        private String dataTime;
+                private String mangName;
+                private String sidoName;
+                private String dataTime;
+            }
+        }
     }
 }
