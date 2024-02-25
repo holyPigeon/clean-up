8f59b0440b0b79bf02881b6aa67fbf31bf485445
jae woo Jung <awfjol2008@naver.com>
2024-02-25 23:18:25 +0900

    feat: 기상청 초단기 실황 API 응답 메시지를 받는 원시 response dto 구현

---
+++ src/main/java/com/kyonggi/cleanup/monitor/application/dto/response/weather/WeatherInfoPrimitiveResponse.java
@@ -0,0 +1,53 @@
+package com.kyonggi.cleanup.monitor.application.dto.response.weather;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class WeatherInfoPrimitiveResponse {
+    private Response response;
+
+    @Getter
+    @NoArgsConstructor
+    public static class Response {
+        private Header header;
+        private Body body;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class Header {
+        private String resultCode;
+        private String resultMsg;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class Body {
+        private String dataType;
+        private Items items;
+        private int pageNo;
+        private int numOfRows;
+        private int totalCount;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class Items {
+        private List<Item> item;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class Item {
+        private String baseDate;
+        private String baseTime;
+        private String category;
+        private int nx;
+        private int ny;
+        private String obsrValue;
+    }
+}
