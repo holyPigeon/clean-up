92c1e36f014d02b83cc88ac84aa6c14734944863
jae woo Jung <awfjol2008@naver.com>
2024-02-27 16:28:46 +0900

    refector: 실시간 날씨 정보 반환 API response dto Builder 패턴 적용

--- src/main/java/com/kyonggi/cleanup/monitor/application/dto/response/weather/WeatherInfoResponse.java
+++ src/main/java/com/kyonggi/cleanup/monitor/application/dto/response/weather/WeatherInfoResponse.java
@@ -1,21 +1,19 @@
 package com.kyonggi.cleanup.monitor.application.dto.response.weather;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@Builder
 public class WeatherInfoResponse {
 
     private String temperature;
     private String humidity;
 
-    private WeatherInfoResponse(String temperature, String humidity) {
-        this.temperature = temperature;
-        this.humidity = humidity;
-    }
-
     public static WeatherInfoResponse of(String temperature, String humidity) {
-        return new WeatherInfoResponse(temperature, humidity);
+        return WeatherInfoResponse.builder()
+                .temperature(temperature)
+                .humidity(humidity)
+                .build();
     }
 }
