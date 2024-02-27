22eee7e6339c2616e0698aea8bce0bb01bc3c575
jae woo Jung <awfjol2008@naver.com>
2024-02-27 16:29:14 +0900

    refector: 주차장 데이터 반환 API response dto Builder 패턴 적용

--- src/main/java/com/kyonggi/cleanup/parkinglot/application/dto/response/ParkingLotDataResponse.java
+++ src/main/java/com/kyonggi/cleanup/parkinglot/application/dto/response/ParkingLotDataResponse.java
@@ -1,28 +1,26 @@
 package com.kyonggi.cleanup.parkinglot.application.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
-@NoArgsConstructor
+@Builder
 public class ParkingLotDataResponse {
 
     private List<Item> items;
 
-    private ParkingLotDataResponse(List<Item> items) {
-        this.items = items;
-    }
-
     public static ParkingLotDataResponse of(List<Item> items) {
-        return new ParkingLotDataResponse(items);
+        return ParkingLotDataResponse.builder()
+                .items(items)
+                .build();
     }
 
     @Getter
-    @NoArgsConstructor
+    @Builder
     public static class Item {
 
         @JsonFormat(pattern = "yyyy.MM.dd HH:mm:ss")
@@ -35,37 +33,17 @@ public class ParkingLotDataResponse {
         private String so2;
         private String no2;
 
-        private Item(
-                LocalDateTime dateTime,
-                String carCount,
-                String temperature,
-                String humidity,
-                String pm10,
-                String pm25,
-                String so2,
-                String no2
-        ) {
-            this.dateTime = dateTime;
-            this.carCount = carCount;
-            this.temperature = temperature;
-            this.humidity = humidity;
-            this.pm10 = pm10;
-            this.pm25 = pm25;
-            this.so2 = so2;
-            this.no2 = no2;
-        }
-
-        public static Item of(
-                LocalDateTime dateTime,
-                String carCount,
-                String temperature,
-                String humidity,
-                String pm10,
-                String pm25,
-                String so2,
-                String no2
-        ) {
-            return new Item(dateTime, carCount, temperature, humidity, pm10, pm25, so2, no2);
+        public static Item of(String[] cellValues) {
+            return Item.builder()
+                    .dateTime(LocalDateTime.parse(cellValues[0]))
+                    .carCount(cellValues[1])
+                    .temperature(cellValues[2])
+                    .humidity(cellValues[3])
+                    .pm10(cellValues[4])
+                    .pm25(cellValues[5])
+                    .so2(cellValues[6])
+                    .no2(cellValues[7])
+                    .build();
         }
     }
 }
