45fcb15281959b8389d918d38c3ddced9d912c3f
jae woo Jung <awfjol2008@naver.com>
2024-05-05 00:30:30 +0900

    feat: 기능 개발

--- src/main/java/com/kyonggi/cleanup/parkinglot/application/ParkingLotService.java
+++ src/main/java/com/kyonggi/cleanup/parkinglot/application/ParkingLotService.java
@@ -1,85 +1,81 @@
 package com.kyonggi.cleanup.parkinglot.application;
 
-import com.kyonggi.cleanup.parkinglot.application.dto.response.ParkingLotDataResponse;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.core.io.ClassPathResource;
+import com.kyonggi.cleanup.parkinglot.application.dto.response.ParkingLotInfoResponse;
+import com.kyonggi.cleanup.parkinglot.application.dto.response.PollutionDataResponse;
+import com.kyonggi.cleanup.parkinglot.domain.ParkingLotData;
+import com.kyonggi.cleanup.parkinglot.infrastructure.ParkingLotInfoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ParkingLotService {
 
-    /*
-    주차장 정보 조회
-    */
-    public ParkingLotDataResponse extractParkingLotData() {
+    private final ParkingLotInfoRepository parkingLotInfoRepository;
 
-        List<ParkingLotDataResponse.Item> items = new ArrayList<>();
-
-        try (InputStream inputStream = new ClassPathResource("data/parking_lot_data.xlsx").getInputStream()) {
-            XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
-            XSSFSheet sheet = workbook.getSheetAt(0);
-            int rows = sheet.getPhysicalNumberOfRows();
-
-            extractDataFromFile(rows, sheet, items);
-
-            workbook.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return ParkingLotDataResponse.of(items);
+    public ParkingLotInfoResponse findParkingLotInfo() {
+        return ParkingLotInfoResponse.of(parkingLotInfoRepository.findAll());
     }
 
-    private void extractDataFromFile(int rows, XSSFSheet sheet, List<ParkingLotDataResponse.Item> items) {
-
-        for (int rowIndex = 1; rowIndex < rows; rowIndex++) {
-            XSSFRow row = sheet.getRow(rowIndex);
-            if (row == null) {
-                continue; // 행이 null이면 다음 행으로 넘어간다.
-            }
-
-            String[] cellValues = new String[8];
-            for (int cellIndex = 0; cellIndex < cellValues.length; cellIndex++) {
-                cellValues[cellIndex] = getValueByCellType(row.getCell(cellIndex));
-            }
-
-            items.add(ParkingLotDataResponse.Item.of(cellValues));
+    public ParkingLotInfoResponse findParkingLotInfoByAmPm(boolean isPm) {
+        List<ParkingLotData> parkingLotInfo = parkingLotInfoRepository.findAll();
+        if (isPm) {
+            return ParkingLotInfoResponse.of(
+                    parkingLotInfo
+                            .stream()
+                            .filter(parkingLotData -> parkingLotData.getHour() >= 12 && parkingLotData.getHour() <= 23)
+                            .toList()
+            );
+        } else {
+            return ParkingLotInfoResponse.of(
+                    parkingLotInfo
+                            .stream()
+                            .filter(parkingLotData -> parkingLotData.getHour() >= 0 && parkingLotData.getHour() <= 11)
+                            .toList()
+            );
         }
     }
 
-    private String getValueByCellType(XSSFCell cell) {
-        String value;
-        switch (cell.getCellType()) {
-            case FORMULA:
-                value = cell.getCellFormula();
-                break;
-            case NUMERIC:
-                value = String.valueOf(cell.getNumericCellValue());
-                break;
-            case STRING:
-                value = cell.getStringCellValue();
-                break;
-            case BLANK:
-                value = "";
-                break;
-            case BOOLEAN:
-                value = String.valueOf(cell.getBooleanCellValue());
-                break;
-            case ERROR:
-                value = String.valueOf(cell.getErrorCellValue());
-                break;
-            default:
-                value = "Unknown Cell Type";
-                break;
-        }
-        return value;
+    public PollutionDataResponse predictPollutionDataByDateTime(
+            int month,
+            int day,
+            int hour,
+            int minute
+    ) {
+
+        // ...
+        // algorithm
+        // ...
+
+        double predictionNox = 1.0;
+        double predictionSox = 2.0;
+
+        return PollutionDataResponse.builder()
+                .nox(predictionNox)
+                .sox(predictionSox)
+                .build();
+    }
+
+    public PollutionDataResponse predictPollutionDataByCondition(
+            int carCount,
+            double temperature,
+            double humidity,
+            double currentNox,
+            double currentSox
+    ) {
+
+        // ...
+        // algorithm
+        // ...
+
+        double predictionNox = 3.0;
+        double predictionSox = 4.0;
+
+        return PollutionDataResponse.builder()
+                .nox(predictionNox)
+                .sox(predictionSox)
+                .build();
     }
 }
---
+++ src/main/java/com/kyonggi/cleanup/parkinglot/domain/ParkingLotData.java
@@ -0,0 +1,39 @@
+package com.kyonggi.cleanup.parkinglot.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "parking_lot_info")
+public class ParkingLotData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int month;
+
+    private int day;
+
+    private int hour;
+
+    private int minute;
+
+    private int carCount;
+
+    private double temperature;
+
+    private double humidity;
+
+    private double nox;
+
+    private double sox;
+
+    private double dieselVehiclePercentage;
+}
